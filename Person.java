d0f1fe7fefb2e10ce42e511948ac3d0fd196a7e4
sakkuchandra <chandrasitiyam@gmail.com>
2024-09-18 11:35:30 +0530

    This is My File

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person
+{
+private String name="Green";
+public void setName(String name)
+{
+  String title="Mr. ";
+  name=title+name;
+}
+public String toString()
+{
+  return name;
+}
+
+}
